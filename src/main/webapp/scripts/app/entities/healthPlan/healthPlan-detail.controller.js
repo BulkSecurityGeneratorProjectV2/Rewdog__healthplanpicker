38265cf3caa26d2db52abc4a636a731463dfe7df
acapobi <acapobi@10.0.0.25>
2015-12-30 00:11:21 -0500

    Moved the annual cost calculation logic into the healthplan controller.
    Starting to add annual costs into simluation details.

--- src/main/webapp/scripts/app/entities/healthPlan/healthPlan-detail.controller.js
+++ src/main/webapp/scripts/app/entities/healthPlan/healthPlan-detail.controller.js
@@ -1,36 +1,40 @@
 'use strict';
 
-angular.module('healthPlanPickerApp')
-    .controller('HealthPlanDetailController', function ($scope, $rootScope, $stateParams, entity, HealthPlan, User) {
-        $scope.healthPlan = entity;
-        $scope.annualCost = function(frequency, premium) {
-        	switch (frequency) {
-            case "weekly":                
-                return premium * 52;
-                break;
-            case "biweekly":
-                return premium * 26;
-                break;
-            case "monthly":
-                return premium * 12;
-                break;
-            case "annually":
-                return premium * 1;
-                break;            
-        }
-        	
-        return value * -1; };
-        
-        
-        
-        $scope.load = function (id) {
-            HealthPlan.get({id: id}, function(result) {
-                $scope.healthPlan = result;
-            });
-        };
-        var unsubscribe = $rootScope.$on('healthPlanPickerApp:healthPlanUpdate', function(event, result) {
-            $scope.healthPlan = result;
-        });
-        $scope.$on('$destroy', unsubscribe);
+angular.module('healthPlanPickerApp').controller(
+		'HealthPlanDetailController',
+		function($scope, $rootScope, $stateParams, entity, HealthPlan, User) {
+			$scope.healthPlan = entity;
 
-    });
+			$scope.load = function(id) {
+				HealthPlan.get({
+					id : id
+				}, function(result) {
+					$scope.healthPlan = result;
+				});
+			};
+			var unsubscribe = $rootScope.$on(
+					'healthPlanPickerApp:healthPlanUpdate', function(event,
+							result) {
+						$scope.healthPlan = result;
+					});
+			$scope.$on('$destroy', unsubscribe);
+			
+			$scope.annualCost = function(){
+				switch ($scope.healthPlan.premiumFrequency) {
+				case "weekly":
+					return $scope.healthPlan.premium * 52;
+					break;
+				case "biweekly":
+					return $scope.healthPlan.premium * 26;
+					break;
+				case "monthly":
+					return $scope.healthPlan.premium * 12;
+					break;
+				case "annually":
+					return $scope.healthPlan.premium * 1;
+					break;
+				}
+				return $scope.healthPlan.premium;
+			};
+
+		});
